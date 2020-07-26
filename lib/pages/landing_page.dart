bf4943a7d28fa5beba36541ab9ecea8f656aee2d
unknown <dmytrotus@gmail.com>
2020-07-26 20:55:30 +0200

    styling

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -7,8 +7,9 @@ class LandingPage extends StatelessWidget{
       color: Colors.greenAccent,
       child: new InkWell(
         child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text('blabla'),
+            new Text('blabla', style: new TextStyle(color:Colors.white, fontSize:50.0, fontWeight: FontWeight.bold)),
             new Text('Tap to Start')
           ]
         )

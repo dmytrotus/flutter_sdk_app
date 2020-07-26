e64bf7264e7cbfbe8451d31280e70594495b064f
unknown <dmytrotus@gmail.com>
2020-07-26 22:34:16 +0200

    add ontap

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -6,11 +6,12 @@ class LandingPage extends StatelessWidget{
     return new Material(
       color: Colors.greenAccent,
       child: new InkWell(
+        onTap: () => print('We tapped  the button'),
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Text('blabla', style: new TextStyle(color:Colors.white, fontSize:50.0, fontWeight: FontWeight.bold)),
-            new Text('Tap to Start')
+            new Text('Lets quizz', style: new TextStyle(color:Colors.white, fontSize:50.0, fontWeight: FontWeight.bold)),
+            new Text('Tap to Start', style: new TextStyle(color:Colors.white70, fontSize:25.0, fontWeight: FontWeight.bold))
           ]
         )
       )

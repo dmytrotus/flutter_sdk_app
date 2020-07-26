c828d91c6980f3eb64441028155028b942dd8629
unknown <dmytrotus@gmail.com>
2020-07-26 23:44:49 +0200

    created true false buttons

---
+++ lib/UI/answer_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget{
+
+  bool answer;
+
+  AnswerButton(this.answer);
+
+  @override
+  Widget build(BuildContext context){
+    return new Expanded(
+              child: new Material(
+                color: answer == true ? Colors.greenAccent : Colors.redAccent,
+                child: new InkWell(
+                  onTap: () => print("You answered true"),
+                  child: new Center(
+                    child: new Container(
+                      child: new Text(answer == true ? "True" : "False")
+                    )
+                  )
+                )
+              )
+            );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sdk_app/pages/quiz_page.dart';
+
 import './pages/landing_page.dart';
+import './pages/quiz_page.dart';
 
 void main() {
   runApp(new MaterialApp(
-    home: new LandingPage(),
+    home: new QuizPage(),
   ));
 }

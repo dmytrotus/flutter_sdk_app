4ba1635baff249e5e2034c17bef7f901ef2b1a76
unknown <dmytrotus@gmail.com>
2020-07-27 18:53:33 +0200

    changed inkwell

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sdk_app/pages/quiz_page.dart';
 
-import './pages/landing_page.dart';
+//import './pages/landing_page.dart';
 import './pages/quiz_page.dart';
 
 void main() {

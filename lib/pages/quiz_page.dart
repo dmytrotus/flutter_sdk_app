74ff947e75f8b3565d94864bf2f95241d6d16dfe
unknown <dmytrotus@gmail.com>
2020-07-28 22:01:56 +0200

    passed questions

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sdk_app/UI/question_text.dart';
 
-//import '../utils/question.dart';
-//import '../utils/quiz.dart';
+import '../utils/question.dart';
+import '../utils/quiz.dart';
 import '../UI/answer_button.dart';
 import '../UI/question_text.dart';
 import '../UI/correct_wrong_overlay.dart';
@@ -14,6 +14,27 @@ class QuizPage extends StatefulWidget {
 }
 
 class QuizPageState extends State<QuizPage> {
+
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question('Катя то мурчик', true),
+    new Question('Катя вредна', true),
+    new Question('Сьогодні був дощ', false)
+  ]);
+
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlaySholdBeVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestion = quiz.nextQuestion;
+    questionText = currentQuestion.question;
+    questionNumber = quiz.questionNumber;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Stack(
@@ -22,11 +43,11 @@ class QuizPageState extends State<QuizPage> {
         new Column( //this is main page
           children: <Widget>[
             new AnswerButton(true, () => print("You anwered True")),
-            new QuestionText("Pizza is super nice", 1),
+            new QuestionText(questionText, questionNumber),
             new AnswerButton(false, () => print('You answered False')),
           ]
         ),
-        new CorrectWrongOverlay(),
+        overlaySholdBeVisible == true ? new CorrectWrongOverlay(true) : new Container(),
       ]
     );
   }
